6f29bfac4eacb5c33c1ecb879355447af7485175
Andrea De Pirro <andrea.depirro@yameveo.com>
2013-02-16 04:35:58 -0800

    Create localEnv.sql

---
+++ localEnv.sql
@@ -0,0 +1,2 @@
+UPDATE `configuration` SET `value` = 'prestashop.dev' WHERE `configuration`.`name` ='PS_SHOP_DOMAIN';
+UPDATE `configuration` SET `value` = 'prestashop.dev' WHERE `configuration`.`name` ='PS_SHOP_DOMAIN_SSL';
