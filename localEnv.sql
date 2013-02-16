ae3834ea14e8dbfdf13b50d68630a8af959a0079
Andrea De Pirro <andrea.depirro@yameveo.com>
2013-02-16 13:44:25 +0100

    Update localEnv.sql

--- localEnv.sql
+++ localEnv.sql
@@ -1,2 +1,8 @@
 UPDATE `configuration` SET `value` = 'prestashop.dev' WHERE `configuration`.`name` ='PS_SHOP_DOMAIN';
 UPDATE `configuration` SET `value` = 'prestashop.dev' WHERE `configuration`.`name` ='PS_SHOP_DOMAIN_SSL';
+UPDATE `configuration` SET `value` = 0 WHERE `configuration`.`name` ='PS_SMARTY_CACHE';
+UPDATE `configuration` SET `value` = 0 WHERE `configuration`.`name` ='PS_CSS_THEME_CACHE';
+UPDATE `configuration` SET `value` = 0 WHERE `configuration`.`name` ='PS_JS_THEME_CACHE';
+UPDATE `configuration` SET `value` = 1 WHERE `configuration`.`name` ='PS_SMARTY_FORCE_COMPILE';
+UPDATE `configuration` SET `value` = 0 WHERE `configuration`.`name` ='PS_HTML_THEME_COMPRESSION';
+UPDATE `configuration` SET `value` = 0 WHERE `configuration`.`name` ='PS_JS_HTML_THEME_COMPRESSION';
